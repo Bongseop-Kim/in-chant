3297540e453feef98c40019d1fea37aa23860234
bongsub kim <bong989971@gmail.com>
2023-08-31 18:52:21 +0900

    asd

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -60,6 +60,7 @@ export class ChatsGateway {
       message: socketObj.message,
       createdAt: socketObj.createdAt,
       userName: socketObj.userName,
+      userId: socketObj.userId,
     });
   }
 }
